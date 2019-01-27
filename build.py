969f79514e6e2b2abfe0d396d8e7f452d6fa58c9
yangjc <bupt_yjc@163.com>
2019-01-27 17:16:26 +0800

    change to trm

--- build.py
+++ build.py
@@ -1,62 +1,178 @@
+import time
+
 import pickle as pk
 
-import numpy as np
+import math
 
-from keras.models import Model
-from keras.layers import Input, Embedding
-from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint
+import torch
+from torch.nn import CrossEntropyLoss
+from torch.optim import Adam
+from torch.utils.data import TensorDataset, DataLoader
 
-from nn_arch import cnn, rnn
+from nn_arch import Trm
 
 from util import map_item
 
 
-batch_size = 512
+def get_pos(seq_len, embed_len):
+    pos = torch.zeros(seq_len, embed_len)
+    for i in range(seq_len):
+        for j in range(embed_len):
+            if j % 2:
+                pos[i, j] = math.sin(i / math.pow(1e4, j / embed_len))
+            else:
+                pos[i, j] = math.cos(i / math.pow(1e4, (j - 1) / embed_len))
+    return torch.unsqueeze(pos, dim=0)
+
+
+def get_mask(head, seq_len):
+    mask = torch.triu(torch.ones(seq_len, seq_len).byte(), diagonal=1)
+    mask = torch.unsqueeze(mask, dim=0)
+    mask = mask.repeat(head, 1, 1)
+    return torch.unsqueeze(mask, dim=0)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+detail = False if torch.cuda.is_available() else True
+
+embed_len = 200
+seq_len = 100
+
+head, stack = 4, 2
+
+batch_size = 32
 
 path_embed = 'feat/embed.pkl'
-path_cnn_sent = 'feat/cnn_sent_train.pkl'
-path_rnn_sent = 'feat/rnn_sent_train.pkl'
-path_label = 'feat/label_train.pkl'
+path_word_ind = 'feat/zh_word_ind.pkl'
 with open(path_embed, 'rb') as f:
     embed_mat = pk.load(f)
-with open(path_cnn_sent, 'rb') as f:
-    cnn_sents = pk.load(f)
-with open(path_rnn_sent, 'rb') as f:
-    rnn_sents = pk.load(f)
-with open(path_label, 'rb') as f:
-    labels = pk.load(f)
+with open(path_word_ind, 'rb') as f:
+    word_inds = pk.load(f)
 
-funcs = {'cnn': cnn,
-         'rnn': rnn}
+mask_mat = get_mask(head, seq_len).to(device)
+pos_mat = get_pos(seq_len, embed_len).to(device)
 
-paths = {'cnn': 'model/cnn.h5',
-         'rnn': 'model/rnn.h5'}
+archs = {'trm': Trm}
+
+paths = {'trm': 'model/dnn_trm.pkl'}
 
 
-def compile(name, embed_mat, seq_len):
-    vocab_num, embed_len = embed_mat.shape
-    embed = Embedding(input_dim=vocab_num, output_dim=embed_len,
-                      weights=[embed_mat], input_length=seq_len, trainable=True)
-    input = Input(shape=(seq_len,))
-    embed_input = embed(input)
-    func = map_item(name, funcs)
-    output = func(embed_input, vocab_num)
-    model = Model(input, output)
-    model.summary()
-    model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
-    return model
+def load_feat(path_feats):
+    with open(path_feats['sent_train'], 'rb') as f:
+        train_sents = pk.load(f)
+    with open(path_feats['label_train'], 'rb') as f:
+        train_labels = pk.load(f)
+    with open(path_feats['sent_dev'], 'rb') as f:
+        dev_sents = pk.load(f)
+    with open(path_feats['label_dev'], 'rb') as f:
+        dev_labels = pk.load(f)
+    return train_sents, train_labels, dev_sents, dev_labels
 
 
-def fit(name, epoch, embed_mat, sents, labels):
-    seq_len = len(sents[0])
-    model = compile(name, embed_mat, seq_len)
-    check_point = ModelCheckpoint(map_item(name, paths), monitor='val_loss', verbose=True, save_best_only=True)
-    labels = np.expand_dims(labels, -1)
-    model.fit(sents, labels, batch_size=batch_size, epochs=epoch,
-              verbose=True, callbacks=[check_point], validation_split=0.2)
+def step_print(step, batch_loss, batch_acc):
+    print('\n{} {} - loss: {:.3f} - acc: {:.3f}'.format('step', step, batch_loss, batch_acc))
+
+
+def epoch_print(epoch, delta, train_loss, train_acc, dev_loss, dev_acc, extra):
+    print('\n{} {} - {:.2f}s - loss: {:.3f} - acc: {:.3f} - val_loss: {:.3f} - val_acc: {:.3f}'.format(
+          'epoch', epoch, delta, train_loss, train_acc, dev_loss, dev_acc) + extra)
+
+
+def tensorize(feats, device):
+    tensors = list()
+    for feat in feats:
+        tensors.append(torch.LongTensor(feat).to(device))
+    return tensors
+
+
+def get_loader(triples):
+    en_sents, zh_sents, labels = triples
+    triples = TensorDataset(en_sents, zh_sents, labels)
+    return DataLoader(triples, batch_size, shuffle=True)
+
+
+def get_metric(model, loss_func, triples):
+    en_sents, zh_sents, labels = triples
+    labels = labels.view(-1)
+    num = (labels > 0).sum().item()
+    prods = model(en_sents, zh_sents)
+    prods = prods.view(-1, prods.size(-1))
+    preds = torch.max(prods, dim=1)[1]
+    loss = loss_func(prods, labels)
+    acc = (preds == labels).sum().item()
+    return loss, acc, num
+
+
+def batch_train(models, loss_func, optim, loader, detail):
+    total_loss, total_acc, total_num = [0] * 3
+    for step, triples in enumerate(loader):
+        batch_loss, batch_acc, batch_num = get_metric(models, loss_func, triples)
+        optim.zero_grad()
+        batch_loss.backward()
+        optim.step()
+        total_loss = total_loss + batch_loss.item()
+        total_acc, total_num = total_acc + batch_acc, total_num + batch_num
+        if detail:
+            step_print(step + 1, batch_loss / batch_num, batch_acc / batch_num)
+    return total_loss / total_num, total_acc / total_num
+
+
+def batch_dev(model, loss_func, loader):
+    total_loss, total_acc, total_num = [0] * 3
+    for step, triples in enumerate(loader):
+        batch_loss, batch_acc, batch_num = get_metric(model, loss_func, triples)
+        total_loss = total_loss + batch_loss.item()
+        total_acc, total_num = total_acc + batch_acc, total_num + batch_num
+    return total_loss / total_num, total_acc / total_num
+
+
+def fit(name, max_epoch, en_embed_mat, zh_embed_mat, path_feats, detail):
+    tensors = tensorize(load_feat(path_feats), device)
+    bound = int(len(tensors) / 2)
+    train_loader, dev_loader = get_loader(tensors[:bound]), get_loader(tensors[bound:])
+    en_embed_mat, zh_embed_mat = torch.Tensor(en_embed_mat), torch.Tensor(zh_embed_mat)
+    arch = map_item(name, archs)
+    model = arch(en_embed_mat, zh_embed_mat, pos_mat, mask_mat, head, stack).to(device)
+    loss_func = CrossEntropyLoss(ignore_index=0, reduction='sum')
+    learn_rate, min_rate = 1e-3, 1e-5
+    min_dev_loss = float('inf')
+    trap_count, max_count = 0, 3
+    print('\n{}'.format(model))
+    train, epoch = True, 0
+    while train and epoch < max_epoch:
+        epoch = epoch + 1
+        model.train()
+        optim = Adam(model.parameters(), lr=learn_rate)
+        start = time.time()
+        train_loss, train_acc = batch_train(model, loss_func, optim, train_loader, detail)
+        delta = time.time() - start
+        with torch.no_grad():
+            model.eval()
+            dev_loss, dev_acc = batch_dev(model, loss_func, dev_loader)
+        extra = ''
+        if dev_loss < min_dev_loss:
+            extra = ', val_loss reduce by {:.3f}'.format(min_dev_loss - dev_loss)
+            min_dev_loss = dev_loss
+            trap_count = 0
+            torch.save(model, map_item(name, paths))
+        else:
+            trap_count = trap_count + 1
+            if trap_count > max_count:
+                learn_rate = learn_rate / 10
+                if learn_rate < min_rate:
+                    extra = ', early stop'
+                    train = False
+                else:
+                    extra = ', learn_rate divide by 10'
+                    trap_count = 0
+        epoch_print(epoch, delta, train_loss, train_acc, dev_loss, dev_acc, extra)
 
 
 if __name__ == '__main__':
-    fit('cnn', 50, embed_mat, cnn_sents, labels)
-    fit('rnn', 50, embed_mat, rnn_sents, labels)
+    path_feats = dict()
+    path_feats['sent_train'] = 'feat/sent_train.pkl'
+    path_feats['label_train'] = 'feat/label_train.pkl'
+    path_feats['sent_dev'] = 'feat/sent_dev.pkl'
+    path_feats['label_dev'] = 'feat/label_dev.pkl'
+    fit('trm', 50, embed_mat, path_feats, detail)
--- nn_arch.py
+++ nn_arch.py
@@ -1,25 +1,119 @@
-from keras.layers import LSTM, Conv1D, Dense, Dropout, Multiply
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-win_len = 10
+class Trm(nn.Module):
+    def __init__(self, en_embed_mat, zh_embed_mat, pos_mat, mask_mat, head, stack):
+        super(Trm, self).__init__()
+        self.encode = TrmEncode(en_embed_mat, pos_mat, head, stack)
+        self.decode = TrmDecode(zh_embed_mat, pos_mat, mask_mat, head, stack)
+
+    def forward(self, x, y):
+        x = self.encode(x)
+        return self.decode(y, x)
 
 
-def cnn(embed_input, vocab_num):
-    conv = Conv1D(filters=128, kernel_size=win_len, padding='valid')
-    gate = Conv1D(filters=128, kernel_size=win_len, padding='valid', activation='sigmoid')
-    da1 = Dense(200, activation='relu')
-    da2 = Dense(vocab_num, activation='softmax')
-    g = gate(embed_input)
-    x = conv(embed_input)
-    x = Multiply()([x, g])
-    x = da1(x)
-    x = Dropout(0.2)(x)
-    return da2(x)
+class TrmEncode(nn.Module):
+    def __init__(self, en_embed_mat, pos_mat, head, stack):
+        super(TrmEncode, self).__init__()
+        en_vocab_num, en_embed_len = en_embed_mat.size()
+        self.en_embed = nn.Embedding(en_vocab_num, en_embed_len, _weight=en_embed_mat)
+        self.pos = pos_mat
+        self.layers = nn.ModuleList([EncodeLayer(en_embed_len, head) for _ in range(stack)])
+
+    def forward(self, x):
+        p = self.pos.repeat(x.size(0), 1, 1)
+        x = self.en_embed(x)
+        x = x + p
+        for layer in self.layers:
+            x = layer(x)
+        return x
 
 
-def rnn(embed_input, vocab_num):
-    ra = LSTM(200, activation='tanh', return_sequences=True)
-    da = Dense(vocab_num, activation='softmax')
-    x = ra(embed_input)
-    x = Dropout(0.2)(x)
-    return da(x)
+class EncodeLayer(nn.Module):
+    def __init__(self, embed_len, head):
+        super(EncodeLayer, self).__init__()
+        self.head = head
+        self.qry = nn.Linear(embed_len, 200 * head)
+        self.key = nn.Linear(embed_len, 200 * head)
+        self.val = nn.Linear(embed_len, 200 * head)
+        self.fuse = nn.Linear(200 * head, 200)
+        self.lal = nn.Sequential(nn.Linear(200, 200),
+                                 nn.ReLU(),
+                                 nn.Linear(200, 200))
+
+    def mul_att(self, x, y):
+        q = self.qry(y).view(y.size(0), y.size(1), self.head, -1).transpose(1, 2)
+        k = self.key(x).view(x.size(0), x.size(1), self.head, -1).transpose(1, 2)
+        v = self.val(x).view(x.size(0), x.size(1), self.head, -1).transpose(1, 2)
+        d = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(k.size(-1))
+        a = F.softmax(d, dim=-1)
+        c = torch.matmul(a, v).transpose(1, 2)
+        c = c.contiguous().view(c.size(0), c.size(1), -1)
+        return self.fuse(c)
+
+    def forward(self, x):
+        r = x
+        x = self.mul_att(x, x)
+        x = F.layer_norm(x + r, x.size()[1:])
+        r = x
+        x = self.lal(x)
+        return F.layer_norm(x + r, x.size()[1:])
+
+
+class TrmDecode(nn.Module):
+    def __init__(self, zh_embed_mat, pos_mat, mask_mat, head, stack):
+        super(TrmDecode, self).__init__()
+        zh_vocab_num, zh_embed_len = zh_embed_mat.size()
+        self.zh_embed = nn.Embedding(zh_vocab_num, zh_embed_len, _weight=zh_embed_mat)
+        self.pos, self.mask = pos_mat, mask_mat
+        self.layers = nn.ModuleList([DecodeLayer(zh_embed_len, head) for _ in range(stack)])
+        self.dl = nn.Sequential(nn.Dropout(0.2),
+                                nn.Linear(200, zh_vocab_num))
+
+    def forward(self, y, x):
+        p, m = self.pos.repeat(y.size(0), 1, 1), self.mask.repeat(y.size(0), 1, 1, 1)
+        y = self.zh_embed(y)
+        y = y + p
+        for layer in self.layers:
+            y = layer(y, x, m)
+        return self.dl(y)
+
+
+class DecodeLayer(nn.Module):
+    def __init__(self, embed_len, head):
+        super(DecodeLayer, self).__init__()
+        self.head = head
+        self.qrys = nn.ModuleList([nn.Linear(embed_len, 200 * head) for _ in range(2)])
+        self.keys = nn.ModuleList([nn.Linear(embed_len, 200 * head) for _ in range(2)])
+        self.vals = nn.ModuleList([nn.Linear(embed_len, 200 * head) for _ in range(2)])
+        self.fuse = nn.Linear(200 * head, 200)
+        self.lal = nn.Sequential(nn.Linear(200, 200),
+                                 nn.ReLU(),
+                                 nn.Linear(200, 200))
+
+    def mul_att(self, x, y, m, i):
+        q = self.qrys[i](y).view(y.size(0), y.size(1), self.head, -1).transpose(1, 2)
+        k = self.keys[i](x).view(x.size(0), x.size(1), self.head, -1).transpose(1, 2)
+        v = self.vals[i](x).view(x.size(0), x.size(1), self.head, -1).transpose(1, 2)
+        d = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(k.size(-1))
+        if i == 0:
+            d = d.masked_fill(m, -float('inf'))
+        a = F.softmax(d, dim=-1)
+        c = torch.matmul(a, v).transpose(1, 2)
+        c = c.contiguous().view(c.size(0), c.size(1), -1)
+        return self.fuse(c)
+
+    def forward(self, y, x, m):
+        r = y
+        y = self.mul_att(y, y, m, 0)
+        y = F.layer_norm(y + r, y.size()[1:])
+        r = y
+        y = self.mul_att(x, y, m, 1)
+        y = F.layer_norm(y + r, y.size()[1:])
+        r = y
+        y = self.lal(y)
+        return F.layer_norm(y + r, y.size()[1:])
